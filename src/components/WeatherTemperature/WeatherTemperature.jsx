151b4a2665965784a13b39bd6ebf853a7ded0f07
Olena <kryvtsovaolena@gmail.com>
2022-11-18 11:40:22 +0200

    Update WeatherTemperature.jsx

--- src/components/WeatherTemperature/WeatherTemperature.jsx
+++ src/components/WeatherTemperature/WeatherTemperature.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Data, Unit } from "./WeatherTemperature.styled";
+
 export function WeatherTemperature({ celsius }) {
-  return (
-    <div>
-      <Data>{Math.round(celsius)}</Data>
-      <Unit>°C</Unit>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  const changeToF = (event) => {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  };
+
+  const changeToC = (event) => {
+    event.preventDefault();
+    setUnit("celsius");
+  };
+
+  if (unit === "celsius") {
+    return (
+      <div>
+        <Data>{Math.round(celsius)}</Data>
+        <Unit>
+          °C |{" "}
+          <a href="/" onClick={changeToF}>
+            {" "}
+            °F
+          </a>
+        </Unit>
+      </div>
+    );
+  } else {
+    let fahTemp = (celsius * 9) / 5 + 32;
+    return (
+      <div>
+        <Data>{Math.round(fahTemp)}</Data>
+        <Unit>
+          <a href="/" onClick={changeToC}>
+            {" "}
+            °C{" "}
+          </a>{" "}
+          | °F
+        </Unit>
+      </div>
+    );
+  }
 }
